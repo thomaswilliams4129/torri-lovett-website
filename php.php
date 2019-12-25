0a608e02ef6814fda255c29c6c41a578a4a7c9c6
thomaswilliams4129 <thwilliams4129@gmail.com>
2019-12-25 13:08:43 -0500

    live on blue host working on contact form

--- php.php
+++ php.php
@@ -1,11 +1,15 @@
 <?php 
-$ToEmail = 'thwilliams4129@gmail.com'; 
-$EmailSubject = 'Site contact form'; 
-$mailheader = "From: ".$_POST["email"]."\r\n"; 
-$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
-$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
-$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
-$MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"]).""; 
-mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
-?>
+$recipient = "thwilliams4129@gmail.com"; 
+$subject = "Site contact form"; 
+$from = "From: ".$_POST['email']."\n"; 
+$replyTo = "Reply-To: ".$_POST['email']."\n"; 
+$name = "Name: ".$_POST['name']."\n"; 
+$style = "Style Shoot: ".$_POST['style']."\n"; 
+$date = "Desired Photography date: ".$_POST['date']."\n";
+$location = "Location: ".$_POST['location']."\n";
+$comment = "Comment: ".$_POST['comment']."\n";
+$MESSAGE_BODY = "$from \n $replyTo \n $name \n $style \n $date \n $location \n $comment \n";
+
+mail($recipient, $subject, $MESSAGE_BODY) or die ("Failure"); 
+echo "Thank you!";
+?> 
